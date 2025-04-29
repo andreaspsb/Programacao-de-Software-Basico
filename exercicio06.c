9051bde00d6ca234dd8746403e82beb869fee618
Andreas Berwaldt <68302791+andreaspsb@users.noreply.github.com>
2025-04-29 23:46:27 +0000

    bits

--- exercicio06.c
+++ exercicio06.c
@@ -1,21 +1,56 @@
 #include <stdio.h>
 
-unsigned int clear(unsigned int val) {
-
+//zera todos os bits
+unsigned int clear(unsigned int val)
+{
 }
 
-unsigned int setbit (unsigned int x, int bit) {
-
+//seta um único bit
+unsigned int setbit(unsigned int x, int bit)
+{
+    //marcar como 1(ligado) o bit em questão
+    unsigned mask = (1 << bit);
+    return x | mask;
 }
 
-unsigned int clearbit (unsigned int x, int bit) {
-
+//resseta um único bit
+unsigned int clearbit(unsigned int x, int bit)
+{
+    //zera um único bit
+    unsigned mask = ~(1 << bit);
+    return x & mask;
 }
 
-unsigned int invertBit (unsigned int x, int bit) {
-
+//inverte o valor de um único bit
+unsigned int invertBit(unsigned int x, int bit)
+{
 }
 
-int testBit (unsigned int x, int bit) {
-    
+//retorna o estado de um determinado bit
+int testBit(unsigned int x, int bit)
+{
+}
+
+int main()
+{
+    unsigned int x = 0xF0F0;
+    printf("Binario: %b\n, x");
+    printf("Decimal: %u\n, x");
+
+    unsigned int var2;
+
+    var2 = clear(x);
+    printf("Clear: %0x\n", var2);
+
+    var2 = setbit(x);
+    printf("Clear: %0x\n", var2);
+
+    var2 = clearbit(x);
+    printf("Clear: %0x\n", var2);
+
+    var2 = invertBit(x);
+    printf("Clear: %0x\n", var2);
+
+    var2 = testBit(x);
+    printf("Clear: %0x\n", var2);
 }
